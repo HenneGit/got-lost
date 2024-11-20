a3e487f14fc208425e3a697506f948f1aa88d552
Henne <henne@localhost.localdomain>
2024-11-20 18:53:59 +0100

    add type to app.

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,8 +2,9 @@ import "@/styles/globals.css";
 
 import {appWithTranslation} from 'next-i18next'
 import Layout from "@/components/Layout/Layout";
+import {AppProps} from "next/app";
 
-const MyApp = ({Component, pageProps}) => (
+const MyApp = ({Component, pageProps} :AppProps) => (
     <Layout>
         <Component {...pageProps} />
     </Layout>
